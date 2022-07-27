e3d0a3a3ee17ac1d81c7c1fbd0665196b16f78ec
SHIVANSHPRIYADARSHI <96731380+SHIVANSHPRIYADARSHI@users.noreply.github.com>
2022-07-27 18:26:54 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ var cars = [];
 
 function preload() {
   backgroundImage = loadImage("./assets/background.png");
-  car1_img = loadImage("./assets/car1.png");
+  car1_img = loadImage("car1.png");
   car2_img = loadImage("./assets/car2.png");
   track = loadImage("./assets/track.jpg");
   fuelImage = loadImage("./assets/fuel.png");
